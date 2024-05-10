7ab37392c229056d38494b319e32adbeed328e36
Basma Sabry <basmasabry33333@gmail.com>
2024-05-11 01:22:37 +0300

    feat: Add database configuration and connection

---
+++ src/config/db.config.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { ConnectOptions } from 'mongoose';
+const faker = require('faker');
+import TripModel from '../models/trip.model';
+
+dotenv.config();
+
+const dbConnect = async (): Promise<void> => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI as string, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        } as ConnectOptions); 
+        console.log('Connected to the database');
+    } catch (error) {
+        console.log('Failed to connect to the database');
+    }
+};
+
+
+async function deleteAllTrips() {
+    try {
+        await TripModel.deleteMany({});
+        console.log('All trips deleted successfully');
+    } catch (error) {
+        console.error('Error deleting all trips:', error);
+    } finally {
+        mongoose.disconnect();
+    }
+}
+
+//deleteAllTrips();
+
+export { dbConnect };
